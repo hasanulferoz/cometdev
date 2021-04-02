2ddf45d9b2fe450f5de1f7cf3e6dfbcca6c1194d
Feroz <Hasan.Feroz@kantar.com>
2021-04-03 03:07:21 +0600

    finised admin template slicing

---
+++ resources/views/admin/layouts/menu.blade.php
@@ -0,0 +1,66 @@
+	<!-- Sidebar -->
+    <div class="sidebar" id="sidebar">
+        <div class="sidebar-inner slimscroll">
+            <div id="sidebar-menu" class="sidebar-menu">
+                <ul>
+                    <li class="menu-title"> 
+                        <span>Main</span>
+                    </li>
+                    <li class="active"> 
+                        <a href="{{route('admin.dashboard')}}"><i class="fe fe-home"></i> <span>Dashboard</span></a>
+                    </li>
+
+
+             
+            
+
+
+                    <li class="menu-title"> 
+                        <span>Pages</span>
+                    </li>
+
+                    <li> 
+                        <a href="profile.html"><i class="fe fe-user-plus"></i> <span>Profile</span></a>
+                    </li>
+                    <li class="submenu">
+                        <a href="#"><i class="fe fe-document"></i> <span> Blog </span> <span class="menu-arrow"></span></a>
+                        <ul style="display: none;">
+                            <li><a href="login.html"> Posts </a></li>
+                            <li><a href="register.html"> Category </a></li>
+                            <li><a href="forgot-password.html"> Tag </a></li>                     
+                        </ul>
+                    </li>
+                    <li class="submenu">
+                        <a href="#"><i class="fe fe-document"></i> <span> Product </span> <span class="menu-arrow"></span></a>
+                        <ul style="display: none;">
+                            <li><a href="login.html"> Product </a></li>
+                            <li><a href="register.html"> Category </a></li>
+                            <li><a href="forgot-password.html"> Brand </a></li>                     
+                        </ul>
+                    </li>
+                    <li class="submenu">
+                        <a href="#"><i class="fe fe-document"></i> <span> Orders </span> <span class="menu-arrow"></span></a>
+                        <ul style="display: none;">
+                            <li><a href="login.html"> Orders </a></li>
+                            <li><a href="register.html"> Reports </a></li>                 
+                        </ul>
+                    </li>
+
+                    <li> 
+                        <a href="profile.html"><i class="fe fe-user-plus"></i> <span>Settings</span></a>
+                    </li>
+                  
+                    <li class="submenu">
+                        <a href="#"><i class="fe fe-document"></i> <span> Users </span> <span class="menu-arrow"></span></a>
+                        <ul style="display: none;">
+                            <li><a href="login.html"> Product </a></li>
+                            <li><a href="register.html"> Category </a></li>
+                            <li><a href="forgot-password.html"> Brand </a></li>                     
+                        </ul>
+                    </li>
+                
+                </ul>
+            </div>
+        </div>
+    </div>
+    <!-- /Sidebar -->
